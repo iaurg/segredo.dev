73e397b76acae4a22aaf74a609eafe43253b1927
iaurg <italoaurelior@gmail.com>
2020-05-08 19:42:18 -0300

    :alembic: Animations on cards

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from '../layout'
 import Helmet from 'react-helmet'
+// Use transition to show posts
+// import {useTransition, animated} from 'react-spring'
 
 export default () => {
   return (
