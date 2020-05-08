779c5f3b1eea1fc465314af4abfaa8c143afba08
iaurg <italoaurelior@gmail.com>
2020-05-08 19:42:56 -0300

    :sparkles: Add new components in home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Layout from '../layout'
+import Quotes from '../components/Quotes'
+import CanvasAnimation from '../components/CanvasAnimation'
 export default () => {
   return (
     <Layout>
+      <Quotes quote='Ter vivido o suficiente não depende nem de nossos anos, nem de
+nossos dias, mas de nossas mentes.'
+      />
+      <CanvasAnimation />
       <div>
         <h4>Posts</h4>
-
       </div>
     </Layout>
   )
